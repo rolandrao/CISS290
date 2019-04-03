92e3b0afe91224701af47d769b22eb9aba7a8104
rolandrao <rolandrao@gmail.com>
2019-04-03 11:19:21 -0400

    Im a bum

---
+++ Class Assignments/ex5-1.cpp
@@ -0,0 +1,22 @@
+/* Roland Rao
+Exercise 5-1 main
+This program will instantiate a rectangle object
+*/
+#include<iostream>
+#include"/Users/rolandrao/Documents/CISS290/Header Files/rectangle5-1.h"
+using namespace std;
+
+int main(){
+  rectangle51 r;
+  float len, wid;
+  cout << "Enter Length: ";
+  cin >> len;
+  cout << "Enter Width: ";
+  cin >> wid;
+  r.setlength(len);
+  r.setwidth(wid);
+  r.calcarea();
+  r.calcperimeter();
+  cout << r.getarea << endl;
+  cout << r.getperimeter << endl;
+}
---
+++ Header Files/rectangle5-1.h
@@ -0,0 +1,50 @@
+#pragma once
+/* Roland Rao
+Exercise 5-1 Header File
+This class will define a rectangle
+*/
+
+class rectangle51{
+private:
+  float length;
+  float width;
+  float area;
+  float perimeter;
+
+public:
+  rectangle()
+  {
+    length = 1;
+    width = 1;
+    area = 1;
+    perimeter = 4;
+  }
+  void setlength(float l){
+    length = l;
+  }
+  void setwidth(float w){
+    width = w;
+  }
+  void calcarea(){
+    area = length*width;
+  }
+  void calcperimeter() {
+    perimeter = 2*(length+width);
+  }
+
+  float getlength(){
+    return length;
+  }
+
+  float getwidth(){
+    return width;
+  }
+
+  float getarea(){
+    return area;
+  }
+
+  float get perimeter(){
+    return perimeter;
+  }
+}
