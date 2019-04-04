66e454c2e77288bef1dfe943c4690ad25397f669
rolandrao <rolandrao@gmail.com>
2019-04-04 11:22:44 -0400

    doing the hybrid and testing stuff

--- Class Assignments/ex4-20.cpp
+++ Class Assignments/ex4-20.cpp
@@ -8,12 +8,18 @@ using namespace std;
 int main(){
   int grades[5] = {87,85,91,95,93};
   int *gptr;
+  gptr= new int[50];
   int *limitptr;
   gptr = grades;
+  if(gptr == limitptr){
+    cout << "True";
+  }else{
+    cout << "False";
+  }
   limitptr = grades + 5;
   while(gptr < limitptr){
     cout << *gptr << " ";
-    gptr++;
+    gptr = gptr +1;
   }
 
 }
